ec59aa44568671c3f0cf8d0f8674b374abde23e9
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-07 20:35:08 -0500

    in step 4 gave button reference

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ const when = ["yesterday", "this morning", "last night", "on Monday"];
 let i1 = 0, i2 = 0, i3 = 0, i4 = 0, i5 = 0;
 //  step 3 Selected words
 let selected = ["", "", "", "", ""];
+//  step 4 Button references
+document.getElementById("part1").addEventListener("click", () => {
+    selected[0] = who[i1];
+    document.getElementById("part1").textContent = selected[0];
+    i1 = (i1 + 1) % who.length;
+});
+document.getElementById("part2").addEventListener("click", () => {
+    selected[1] = didWhat[i2];
+    document.getElementById("part2").textContent = selected[1];
+    i2 = (i2 + 1) % didWhat.length;
+});
+
+document.getElementById("part3").addEventListener("click", () => {
+    selected[2] = withWhat[i3];
+    document.getElementById("part3").textContent = selected[2];
+    i3 = (i3 + 1) % withWhat.length;
+});
+
+document.getElementById("part4").addEventListener("click", () => {
+    selected[3] = where[i4];
+    document.getElementById("part4").textContent = selected[3];
+    i4 = (i4 + 1) % where.length;
+});
+
+document.getElementById("part5").addEventListener("click", () => {
+    selected[4] = when[i5];
+    document.getElementById("part5").textContent = selected[4];
+    i5 = (i5 + 1) % when.length;
+});
