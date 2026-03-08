414c8372dfd8575db023ce08e1f38e786deee9e6
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-07 20:34:29 -0500

    step 2 and 3 added to js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const didWhat = ["ate", "jumped over", "played with", "chased"];
 const withWhat = ["spaghetti", "a ball", "a frog", "a sandwich"];
 const where = ["at the park", "in the castle", "on the moon", "at school"];
 const when = ["yesterday", "this morning", "last night", "on Monday"];
+//  step 2 : Added Index trackers
+let i1 = 0, i2 = 0, i3 = 0, i4 = 0, i5 = 0;
+//  step 3 Selected words
+let selected = ["", "", "", "", ""];
