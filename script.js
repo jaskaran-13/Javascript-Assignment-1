6947cf61af215e5680989361f65b3640eb44608b
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-07 20:33:41 -0500

    // step 1 : array for each part of sentence in js

---
+++ script.js
@@ -0,0 +1,6 @@
+// step 1 : array for each part of sentence
+const who = ["The funny monkey", "A brave cate", "My little sister", "A sleepy elephant"];
+const didWhat = ["ate", "jumped over", "played with", "chased"];
+const withWhat = ["spaghetti", "a ball", "a frog", "a sandwich"];
+const where = ["at the park", "in the castle", "on the moon", "at school"];
+const when = ["yesterday", "this morning", "last night", "on Monday"];
